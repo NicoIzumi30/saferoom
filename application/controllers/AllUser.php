fc88d1705ac8589325367a32874f1a41d8c2c81a
Fajar Permana <115998230+fapro84@users.noreply.github.com>
2022-11-03 15:14:02 +0700

    heru pedo

--- application/controllers/AllUser.php
+++ application/controllers/AllUser.php
@@ -6,10 +6,10 @@ class AllUser extends CI_Controller
     public function index()
     {
         $data['title'] = 'All User';
-        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        // $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         $data['usere'] = $this->M_users->getUsers();
         $this->load->view('template/header', $data);
         $this->load->view('user/index', $data);
-        $this->load->view('template/footer');
+        // $this->load->view('template/footer');
     }
 }
